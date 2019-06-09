64277f6bb0d301ba3f246773174cdc0ae8e7d60c
kahina <kahinarahmani0@gmail.com>
2019-06-09 22:52:32 +0200

    infini bridges + everest

---
+++ Assets/Scripts/EverestManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+
+public class EverestManager : MonoBehaviour
+{
+    public GameObject[] everestPrefabs;
+    private Transform playerTransform;
+    private float spawnZ = -150.0f; // la nouvelle position Z du nouveau everest à ajouter
+    private float everestLength = 300.0f;
+    private float safeZone = 200.0f;// la zone où on peut supprimer les everest
+    private int nbtilesOnScreen = 3;
+
+    private List<GameObject> activeEverests;
+ 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        activeEverests = new List<GameObject>();
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        for (int i = 0; i < nbtilesOnScreen; i++)
+        {
+            SpawnEverest();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerTransform.position.z - safeZone > (spawnZ - nbtilesOnScreen * everestLength))
+        {
+            SpawnEverest();
+            deleteEverest();
+        }
+    }
+
+    //ajouter des objets everest
+    private void SpawnEverest(int prefabIndex = -1)
+
+    {
+        GameObject go;
+        //creer un nouveau everest
+        go = Instantiate(everestPrefabs[0]) as GameObject;
+        go.transform.SetParent(transform);
+        //déplacer le nouveau everest
+        go.transform.position = Vector3.forward * spawnZ;
+        // MAJ spawnZ
+        spawnZ += everestLength;
+        activeEverests.Add(go);
+
+    }
+     //supprimer les premiers objets everest créés pour ne pas avoir out of memory
+    private void deleteEverest()
+    {
+        //détruire le premier everest de la liste
+        Destroy(activeEverests[0]);
+        activeEverests.RemoveAt(0);
+    }
+
+}
--- Assets/Scripts/TileManager.cs
+++ Assets/Scripts/TileManager.cs
@@ -1,24 +1,119 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+
 
 public class TileManager : MonoBehaviour
 {
     public GameObject[] tilePrefabs;
     private Transform playerTransform;
-    private float spawnZ = 0.0f;
-    private float tileLength = 10.0f;
-    private int nbtiles = 7;
+    private float spawnZ = -10.0f; // la nouvelle position Z du nouveau bridge à ajouter
+    private float tileLength;
+    private float safeZone = 50.0f;
+    private int nbtilesOnScreen = 15;
+    //private int prefIndex;
+
+    private int lastPrefabIndex = 0;
+    private List<GameObject> activeTiles;
 
     // Start is called before the first frame update
     void Start()
     {
+        activeTiles = new List<GameObject>();
         playerTransform = GameObject.FindGameObjectWithTag ("Player").transform;
+        for(int i = 0 ; i < nbtilesOnScreen; i++)
+        {
+            if(i < 2)
+            {
+                SpawnTile(0);
+            }
+            else
+                SpawnTile();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (playerTransform.position.z - safeZone > (spawnZ - nbtilesOnScreen * tileLength))
+        {
+            SpawnTile();
+            deleteTiles();
+        }
     }
+
+    private void SpawnTile(int prefabIndex = -1)
+
+    {
+        GameObject go;
+        if (prefabIndex == -1)
+        {
+            //creer un bridge de l'index choisi
+            int randomPrefIndex = RandomPrefabIndex();
+            go = Instantiate(tilePrefabs[randomPrefIndex]) as GameObject;
+            tileLength = PrefabLength(randomPrefIndex);
+        }
+        else
+        {
+            go = Instantiate(tilePrefabs[prefabIndex]) as GameObject;
+            tileLength = PrefabLength(prefabIndex);
+        }
+        
+        go.transform.SetParent(transform);
+        //déplacer le nouveau bridge
+        go.transform.position = Vector3.forward * spawnZ;
+        // MAJ spawnZ
+        spawnZ += tileLength;
+        activeTiles.Add(go);
+
+    }
+
+    //supprimer les premiers bridges créés pour ne pas avoir out of memory
+    private void deleteTiles()
+    {
+        //détruire le premier bridge de la liste
+        Destroy(activeTiles[0]);
+        activeTiles.RemoveAt(0);
+    }
+
+    private int RandomPrefabIndex()
+    {
+        if (tilePrefabs.Length <= 1)
+        {
+            return 0;
+        }
+       
+        int randomIndex = lastPrefabIndex;
+        while(randomIndex == lastPrefabIndex)
+        {
+            randomIndex = UnityEngine.Random.Range(0, tilePrefabs.Length);
+        }
+        lastPrefabIndex = randomIndex;
+        return randomIndex;
+    }
+
+     private int PrefabLength(int prefabIndex)
+     {
+         int prefabLength  = 0; 
+         switch(prefabIndex)
+         {
+             case 0:
+                 prefabLength = 10;
+                 break;
+             case 1:
+                prefabLength = 50;
+                 break;
+             case 2:
+                 prefabLength = 60;
+                 break;
+             case 3:
+                prefabLength = 95;
+                 break;
+             case 4:
+                 prefabLength = 35;
+                 break;
+         }
+         return prefabLength;
+     }
 }
